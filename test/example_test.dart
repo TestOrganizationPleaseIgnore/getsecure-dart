ff2662422615e820cff9d341965aa3fe31225914
Marat Shakirov <marat.s@affinidi.com>
2025-03-25 18:05:48 +0100

    fix: add real test

--- test/example_test.dart
+++ test/example_test.dart
@@ -1,15 +1,30 @@
 import 'package:test/test.dart';
+import 'package:crypto/crypto.dart';
+import 'dart:convert';
+import 'package:getsecure/getsecure.dart'; // Adjust this import based on the actual location
 
 void main() {
-  group('A group of tests', () {
-    // final awesome = Awesome();
+  group('secureLink', () {
+    test('generates a valid secure link with MD5 hash and expiration', () {
+      String baselink = 'https://example.com/resource';
+      String secret = 'my_secret_key';
+      int period = 30;
 
-    setUp(() {
-      // Additional setup goes here.
-    });
+      String result = secureLink(baselink, secret, period: period);
+      Uri uri = Uri.parse(result);
 
-    test('First Test', () {
-      // expect(awesome.isAwesome, isTrue);
+      expect(uri.queryParameters.containsKey('md5'), isTrue);
+      expect(uri.queryParameters.containsKey('expires'), isTrue);
+
+      int expires = int.parse(uri.queryParameters['expires']!);
+      expect(expires, greaterThan(DateTime.now().millisecondsSinceEpoch ~/ 1000));
+
+      // Verify MD5 hash
+      String expectedHashString = '$expires${Uri.parse(baselink).path} $secret';
+      List<int> expectedBytes = utf8.encode(expectedHashString);
+      Digest expectedDigest = md5.convert(expectedBytes);
+      String expectedProtectionString = base64Url.encode(expectedDigest.bytes).replaceAll('=', '');
+      expect(uri.queryParameters['md5'], expectedProtectionString);
     });
   });
 }
